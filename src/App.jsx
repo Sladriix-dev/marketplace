0ca9a9f607e5542a9fd7bc21739c402869d5ebac
clannou <48106215+clannou@users.noreply.github.com>
2022-03-23 11:47:01 +0100

    added BestProducts component on root page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/home/Home";
+/*import Home from "./components/home/Home";*/
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Product from './components/Products/Product/Product';
 import Products from "./components/Products/Products";
+
+import BestProducts from "./components/bestProducts/BestProducts";
+
 
 function App () {
     return (
         <div className="App">
             <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<BestProducts />} />
                 <Route path="/products" element={<Products />} />
                 <Route path="/products/:id" element={<Product />} />
             </Routes>
+
+            
+
             <Footer />
         </div>
     );
---
+++ src/components/bestProducts/BestProducts.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './bestProducts.css';
+
+const BestProducts = () => {
+  return (
+    <div>
+      
+      <h1 class="h1-title">Retrouvez nos derniers produits !</h1>
+
+      <div class="separator"></div>
+
+      <div class="three-product-inline">
+
+        <div class="product-inline">
+
+          <img class="product-image" src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&w=1000&q=80" width="150px" height="150px"></img>
+          <div class="product-title">Product Title</div>
+          <div class="product-description">Product Description</div>
+          <div class="product-price">14.99 €</div>
+          <button class="buy-product-button">Acheter</button>
+
+        </div>
+
+        <div class="product-inline">
+
+          <img class="product-image" src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&w=1000&q=80" width="150px" height="150px"></img>
+          <div class="product-title">Product Title</div>
+          <div class="product-description">Product Description</div>
+          <div class="product-price">14.99 €</div>
+          <button class="buy-product-button">Acheter</button>
+
+        </div>
+
+        <div class="product-inline">
+
+          <img class="product-image" src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8&w=1000&q=80" width="150px" height="150px"></img>
+          <div class="product-title">Product Title</div>
+          <div class="product-description">Product Description</div>
+          <div class="product-price">14.99 €</div>
+          <button class="buy-product-button">Acheter</button>
+
+        </div>
+
+      </div>  
+      
+      </div>
+
+    
+  )
+}
+
+export default BestProducts
